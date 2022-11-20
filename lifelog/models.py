d6ffd841ad36ef14cd7f9b04b8c179563f254d42
kettle <teacues@gmail.com>
2022-11-20 22:28:38 +0900

    DRFの調整

--- lifelog/models.py
+++ lifelog/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
 
 class Lifelog(models.Model):
-    date = models.DateTimeField(auto_now_add=True)
-    event = models.CharField(blank=True, default='', max_length=100)
+    date = models.DateTimeField(default=timezone.now)
+    event = models.CharField(blank=True, null=True, max_length=100)
     created_by = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         verbose_name='ユーザー',
         on_delete=models.CASCADE
     )
+    created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
--- lifelog/serializers.py
+++ lifelog/serializers.py
@@ -4,8 +4,6 @@ from .models import Lifelog
 class LifelogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lifelog
-        fields = (
-            'id',
-            'event',
-            'created_by',
-        )
+        
+        # すべてのフィールドをシリアライズ
+        exclude = ()
