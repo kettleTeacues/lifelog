8ff87a8ea400689ba7357335adc54e9a63e9b027
kettle <teacues@gmail.com>
2022-11-20 17:43:18 +0900

    setup

--- lifelog/models.py
+++ lifelog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Lifelog(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    event = models.CharField(blank=True, default='', max_length=100)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name='ユーザー',
+        on_delete=models.CASCADE
+    )
+    update_at = models.DateTimeField(auto_now=True)
--- lifelog/views.py
+++ lifelog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_safe
 
-# Create your views here.
+from lifelog.models import Lifelog
+
+@require_safe
+def index(request):
+    lifelog = Lifelog.objects.all()
+    context = {'lifelog': lifelog}
+    return render(request, 'lifelog/index.html', context)
