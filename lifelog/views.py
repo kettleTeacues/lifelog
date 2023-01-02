9015785ec0dd7bbba42241054498b654f14976fd
kettleTeacues <teacues@gmail.com>
2023-01-02 23:53:24 +0900

    lifelog.indexの表示にvueを実装

--- lifelog/views.py
+++ lifelog/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_safe
 
+import json
+
 from lifelog.models import Lifelog
+from lifelog.serializers import LifelogSerializer
 
 @login_required
 def index(request):
     lifelog = Lifelog.objects.order_by('-staDate').filter(created_by=request.user.id)
-    context = {'lifelog': lifelog}
+    serializer = LifelogSerializer(lifelog, many=True)
+
+    context = {'lifelog': json.dumps(serializer.data, ensure_ascii=False)}
     return render(request, 'lifelog/index.html', context)
 
 @login_required
