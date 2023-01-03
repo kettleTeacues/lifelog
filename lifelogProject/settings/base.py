58ca66f9b1e9933d327ec355c98365bfe1c7eb31
kettleTeacues <teacues@gmail.com>
2023-01-03 20:59:56 +0900

    lifelog.indexの一覧をテキストに変更

--- lifelogProject/settings/base.py
+++ lifelogProject/settings/base.py
@@ -66,6 +66,9 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
+        'OPTIONS': {
+            'options': os.getenv('DB_SCHEMA')
+        }
     }
 }
 
