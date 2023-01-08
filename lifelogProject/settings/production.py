a60c2f3f99a789025f65a1fd2f3242a03b6890ca
kettleTeacues <teacues@gmail.com>
2023-01-08 11:46:56 +0900

    STATIC, MEDIA _ROOTをbase.pyに記載

--- lifelogProject/settings/production.py
+++ lifelogProject/settings/production.py
@@ -4,6 +4,3 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'lifelog.piechika.com']
 CSRF_TRUSTED_ORIGINS = ['https://lifelog.piechika.com']
-
-STATIC_ROOT = '/usr/share/nginx/html/static'
-MEDIA_ROOT = '/usr/share/nginx/html/media'
