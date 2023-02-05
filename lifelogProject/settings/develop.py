9d4b628f84d120b4c0f0e30e9f2edb5836f11ef6
kettleTeacues <teacues@gmail.com>
2023-02-06 00:47:46 +0900

    デプロイ用に最適化

--- lifelogProject/settings/develop.py
+++ lifelogProject/settings/develop.py
@@ -5,10 +5,6 @@ ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 INSTALLED_APPS += [
     'corsheaders',
 ]
