268a90e8556ae6b766b8ec0acf143358873be017
kettleTeacues <teacues@gmail.com>
2023-02-12 23:09:42 +0900

    ルーティングに当てはまらないURLの制御を実装

--- lifelogProject/urls.py
+++ lifelogProject/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.urls.exceptions import NoReverseMatch
+
+def base_view(request):
+        return render(request, 'index.html')
 
 urlpatterns = [
     path('', include('lifelog.urls')),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
+    re_path('^.*$', base_view)
+    
  ]
